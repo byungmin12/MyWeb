409f61320fcaec534bd5bd2d1cac22b1b7c3a5f4
byungmin12 <byungminkim12@gmail.com>
2021-09-15 17:46:36 +0900

    make my web site wireframe

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+
+const AppBody = styled.div`
+  width: 100vw;
+  height: 100vh;
+`;
+
+const ScreenSection = styled.div`
+  width: 100%;
+  height: 100%;
+  /* background-color: red; */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ScreenBody = styled.div`
+  width: 70%;
+  height: 70%;
+  background-color: grey;
+  text-align: center;
+`;
+
+const ProjectSection = styled.div`
+  /* background-color: blue; */
+  position: fixed;
+  top: 100%;
+  left: 65%;
+  width: 35%;
+  height: 20%;
+  transform: translate(0%, -100%);
+`;
+
+const ProjectBody = styled.div`
+  display: inline-block;
+  width: 100%;
+  height: 100%;
+  background-color: white;
+  clip-path: polygon(12% 23%, 73% 0%, 100% 54%, 100% 100%, 12% 100%);
+  position: relative;
+`;
+
+const ProjectLeft = styled.div`
+  background-color: #d3d3d3;
+  clip-path: polygon(12% 23%, 12% 23%, 37% 100%, 12% 100%);
+  width: 100%;
+  height: 100%;
+`;
+
+const Button = styled.div`
+  position: fixed;
+  background-color: red;
+  width: 30px;
+  height: 30px;
+  top: 50%;
+  left: 50%;
+  z-index: 999;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppBody>
+      <ScreenSection>
+        <ScreenBody>바보</ScreenBody>
+      </ScreenSection>
+      <ProjectSection>
+        <ProjectBody>
+          <ProjectLeft></ProjectLeft>
+          <Button>ddd</Button>
+        </ProjectBody>
+      </ProjectSection>
+    </AppBody>
   );
 }
 
