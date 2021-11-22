70d109f0b65b874b4910e98c62c939df7d949605
byungmin12 <kbm940526@naver.com>
2021-11-23 06:25:49 +0900

    add components GradientPage , myinfopage , projects and skillpage

--- src/App.js
+++ src/App.js
@@ -1,65 +1,48 @@
 import React, { useState, useRef, useCallback, useEffect, Suspense } from 'react';
-import { Canvas, useFrame, useThree } from '@react-three/fiber';
-import { OrbitControls, Text } from '@react-three/drei';
-import styled from 'styled-components';
+import styled, { keyframes, css } from 'styled-components';
 import CanvasPage from './page/CanvasPage';
+import Fade from 'react-reveal/Fade';
+import GradientPage from './page/GradientPage';
+import MyInfoPage from './page/MyInfoPage';
+import SkillPage from './page/SkillPage';
+import Projects from './page/Projects';
 
 const AppCss = styled.div`
-  overflow: scroll;
+  height: 100vh;
+  overflow-x: hidden;
+  overflow-y: scroll;
 `;
 
-const SkillPage = styled.div`
+const SkillPageS = styled.div`
   width: 100vw;
-  height: 400vh;
-  background-color: red;
-  border: 3px solid black;
+  height: 100vh;
+  scroll-snap-align: center;
+  font-size: 24px;
+  position: relative;
+  .bold {
+    font-weight: 800;
+    color: rgb(104, 82, 242);
+  }
 `;
 
 function App() {
-  // const { scrollYProgress } = useViewportScroll();
-  // const yRange = useTransform(scrollYProgress, [0, 0.9], [0, 1]);
-  // const pathLength = useSpring(yRange, { stiffness: 400, damping: 90 });
-  const testRef = useRef();
-  const testRef1 = useRef();
+  const [position, setPosition] = useState();
+  const scrollArea = useRef();
+  const onScroll = (e) => setPosition(e.target.scrollTop);
 
-  const handleScroll = () => {
-    console.log(testRef);
-    console.log(window.pageYOffset); //전체 중 위치
-    console.log(testRef.current.clientHeight); // 현재 div 높이
-    console.log(testRef.current.offsetTop); // 부모에서부터 값
-    console.log(testRef1.current.offsetTop); // 부모에서부터 값
-  };
-
-  useEffect(() => {
-    console.log(window.pageYOffset + 1); //전체 중 위치
-    console.log(testRef);
-    console.log(testRef.current.clientHeight); // 현재 div 높이
-    console.log(testRef.current.offsetTop); // 부모에서부터 값
-    if (testRef) {
-      window.addEventListener('scroll', handleScroll);
-    }
-  }, []);
   return (
-    <AppCss>
+    <AppCss ref={scrollArea} onScroll={onScroll}>
       <CanvasPage />
-      <SkillPage ref={testRef}>
-        <div></div>
-      </SkillPage>
-      <SkillPage ref={testRef1}>
-        <div></div>
-      </SkillPage>
-      <SkillPage>
-        <div></div>
-      </SkillPage>
-      <SkillPage>
-        <div></div>
-      </SkillPage>
-      <SkillPage>
-        <div></div>
-      </SkillPage>
-      <SkillPage>
-        <div>asdasd</div>
-      </SkillPage>
+      <GradientPage position={position} />
+      <MyInfoPage />
+      <SkillPage />
+      <Projects />
+      {/* <SkillPageS>
+        <div>
+          <div>이런 회사를 찾습니다.</div>
+          <div>asd</div>
+        </div>
+      </SkillPageS> */}
     </AppCss>
   );
 }
