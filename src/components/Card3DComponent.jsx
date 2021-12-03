e76470b167a37e0cfa89c9c97a21889b651b2608
byungmin12 <kbm940526@naver.com>
2021-12-03 16:16:13 +0900

    add and update new 3d Card component

---
+++ src/components/Card3DComponent.jsx
@@ -0,0 +1,202 @@
+import React, { useState, useEffect, useRef } from 'react';
+import styled from 'styled-components';
+
+const InnerContainer = styled.div`
+  width: 100%;
+  min-height: 80%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  transform-style: preserve-3d;
+  transition: transform 1s;
+  ${(props) => `transform : rotateX(${props.angle}deg);`}
+`;
+
+const Card3D = styled.div`
+  position: absolute;
+  width: 700px;
+  height: 80%;
+  min-height: 500px;
+  transform: ${(props) => `rotateX(${props.angle}deg) translateZ(${props.transZ + 25}px)`};
+`;
+
+const GifContainer = styled.div`
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  backface-visibility: hidden;
+  transition: 1s;
+  border: 3px solid black;
+
+  img {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+const GifTextContainer = styled.div`
+  height: calc(100% - (20px));
+  width: 100%;
+  padding: 20px 20px 0px 20px;
+  background-color: white;
+  position: absolute;
+  top: 0;
+  left: 0;
+  transform: rotateY(-180deg);
+  backface-visibility: hidden;
+  transition: 1s;
+  border: 3px solid black;
+
+  .title {
+    font-size: 48px;
+    font-weight: 1000;
+    width: 100%;
+    text-align: center;
+    color: rgb(104, 82, 242);
+  }
+  .explain {
+    font-size: 16px;
+    margin-top: 12px;
+  }
+  .stackContainer {
+    width: 100%;
+    height: auto;
+    margin-top: 12px;
+    display: flex;
+    justify-content: space-between;
+    .stack {
+      width: auto;
+      height: auto;
+      font-size: 16px;
+      font-weight: 800;
+      border-radius: 12px;
+      background-color: rgb(104, 82, 242);
+      color: white;
+      padding: 5px;
+      white-space: nowrap;
+      margin-right: 5px;
+    }
+  }
+`;
+
+const Card3DContainer = styled.div`
+  display: flex;
+  height: 100%;
+  width: 100%;
+  position: relative;
+  perspective: 1000px;
+  transition: 1s;
+
+  :hover {
+    ${GifContainer} {
+      transform: rotateY(180deg);
+    }
+    ${GifTextContainer} {
+      transform: rotateY(0deg);
+    }
+  }
+`;
+
+function Card3DComponent({ idx }) {
+  const ProjectsData = [
+    {
+      url: '../../mywebsize.gif',
+      title: 'PORTFOLIO',
+      explain: '순수 CSS와 three.js를 활용하여 나를 소개할 수 있는 웹 사이트',
+      stack: ['React', 'STYLED COMPONENTS', 'JS', 'three.js'],
+      contribution: ['웹 전체적인 디자인 구축', '각종 Animation 지정', '3d 모델링'],
+      repository: [
+        { where: '배포', url: 'byungmin.kim' },
+        { where: '깃헙', url: 'https://github.com/byungmin12/MyWeb' },
+      ],
+    },
+    {
+      url: '../../isthisright.gif',
+      title: '이거맞아?',
+      explain: '운동을 좋아하는 사람들을 위한 운동 피드백 커뮤니티 ',
+      stack: ['React', 'STYLED COMPONENTS', 'JS'],
+      contribution: [
+        'Landing page 구현',
+        'Nav bar 구현',
+        'Question page 구현',
+        'Login page 구현',
+        'Sign up page 구현',
+        'Mypage 구현',
+        'footer 구현',
+      ],
+      repository: [
+        { where: '배포', url: 'isthisright.kr' },
+        { where: '깃헙', url: 'https://github.com/codestates/is-this-right' },
+        { where: '기획서', url: 'https://codestates.notion.site/1-S-M-S-31dbe334fe7544b79569c717c8c6bb16' },
+      ],
+    },
+    {
+      url: '../../mysubway.gif',
+      title: 'MySubway',
+      explain:
+        '나만의 서브웨이 메뉴를 만들어 저장하고 사람들과 공유합니다. 메뉴 추천을 통해 서브웨이 메뉴 고민을 줄여주는 웹페이지',
+      stack: ['React', 'STYLED COMPONENTS', 'JS'],
+      contribution: [
+        '웹 전체적인 디자인 구축',
+        'Side bar 구현',
+        'post page 구현',
+        'detail page 구현',
+        'post card 구현',
+      ],
+      repository: [
+        { where: '깃헙', url: 'https://github.com/codestates/My-subway' },
+        { where: '기획서', url: 'https://github.com/codestates/My-subway/wiki' },
+      ],
+    },
+  ];
+  const ref = useRef();
+  const [tz, setTz] = useState(0);
+  const [rotateY, setRotateY] = useState(0);
+  const [angle, setAngle] = useState(0);
+
+  useEffect(() => {
+    if (ref) {
+      const value = Math.round(ref.current.offsetHeight / 2 / Math.tan(Math.PI / ProjectsData.length));
+      setTz(value);
+      setRotateY(360 / ProjectsData.length);
+    }
+  }, []);
+
+  useEffect(() => {
+    let angleValue = (idx / ProjectsData.length) * -360;
+    setAngle(angleValue);
+  }, [idx]);
+
+  return (
+    <InnerContainer angle={angle} transZ={tz}>
+      {ProjectsData.map((el, key) => {
+        return (
+          <Card3D ref={ref} key={key} angle={`${rotateY}` * key} transZ={tz}>
+            <Card3DContainer>
+              <GifContainer>
+                <img src="../../mysubway.gif" />
+              </GifContainer>
+              <GifTextContainer>
+                <div className="title">My Subway</div>
+                <div className="explain">
+                  '나만의 서브웨이 메뉴를 만들어 저장하고 사람들과 공유합니다. 메뉴 추천을 통해 서브웨이 메뉴 고민을
+                  줄여주는 웹페이지'
+                </div>
+                <div className="stackContainer">
+                  {ProjectsData[0].stack.map((el) => {
+                    return <span className="stack">{el}</span>;
+                  })}
+                </div>
+              </GifTextContainer>
+            </Card3DContainer>
+          </Card3D>
+        );
+      })}
+    </InnerContainer>
+  );
+}
+
+export default Card3DComponent;
--- src/page/Projects.jsx
+++ src/page/Projects.jsx
@@ -1,8 +1,9 @@
 import React, { useRef, useEffect, useState } from 'react';
 import styled from 'styled-components';
 import Card from '../components/Card';
+import Card3DComponent from '../components/Card3DComponent';
 
-const SkillPageS = styled.div`
+const ProjectPage = styled.div`
   width: 100vw;
   min-height: 65vh;
   height: 65vh;
@@ -18,7 +19,9 @@ const SkillPageS = styled.div`
 
 const ProjectsContainer = styled.div`
   width: 80%;
-  height: 60%;
+  height: 80%;
+  min-height: 600px;
+
   padding: 60px;
   border: 3px dashed rgb(104, 82, 242);
   position: relative;
@@ -44,19 +47,6 @@ const ProjectsContainer = styled.div`
   }
 `;
 
-const InnerContainer = styled.div`
-  width: 100%;
-  min-height: 80%;
-  height: 100px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  position: absolute;
-  transform-style: preserve-3d;
-  transition: transform 1s;
-  ${(props) => `transform : rotateX(${props.angle}deg);`}
-`;
-
 const ArrowLeft = styled.div`
   position: absolute;
   left: -2%;
@@ -70,98 +60,14 @@ const ArrowRight = styled(ArrowLeft)`
   left: 101%;
 `;
 
-const Card3D = styled.div`
-  position: absolute;
-  width: 80%;
-  height: 80%;
-  background-color: blue;
-  transform: ${(props) => `rotateX(${props.angle}deg) translateZ(${props.transZ + 25}px)`};
-`;
-
 function Projects({}) {
-  const ProjectsData = [
-    {
-      url: '../../mywebsize.gif',
-      title: 'PORTFOLIO',
-      explain: '순수 CSS와 three.js를 활용하여 나를 소개할 수 있는 웹 사이트',
-      stack: ['React', 'STYLED COMPONENTS', 'JS', 'three.js'],
-      contribution: ['웹 전체적인 디자인 구축', '각종 Animation 지정', '3d 모델링'],
-      repository: [
-        { where: '배포', url: 'byungmin.kim' },
-        { where: '깃헙', url: 'https://github.com/byungmin12/MyWeb' },
-      ],
-    },
-    {
-      url: '../../isthisright.gif',
-      title: '이거맞아?',
-      explain: '운동을 좋아하는 사람들을 위한 운동 피드백 커뮤니티 ',
-      stack: ['React', 'STYLED COMPONENTS', 'JS', 'Node.js', 'Sequelize', 'Multer', 'axios', 'Socket.io'],
-      contribution: [
-        'Landing page 구현',
-        'Nav bar 구현',
-        'Question page 구현',
-        'Login page 구현',
-        'Sign up page 구현',
-        'Mypage 구현',
-        'footer 구현',
-      ],
-      repository: [
-        { where: '배포', url: 'isthisright.kr' },
-        { where: '깃헙', url: 'https://github.com/codestates/is-this-right' },
-        { where: '기획서', url: 'https://codestates.notion.site/1-S-M-S-31dbe334fe7544b79569c717c8c6bb16' },
-      ],
-    },
-    {
-      url: '../../mysubway.gif',
-      title: 'MySubway',
-      explain:
-        '나만의 서브웨이 메뉴를 만들어 저장하고 사람들과 공유합니다. 메뉴 추천을 통해 서브웨이 메뉴 고민을 줄여주는 웹페이지',
-      stack: ['React', 'STYLED COMPONENTS', 'JS', 'Node.js'],
-      contribution: [
-        '웹 전체적인 디자인 구축',
-        'Side bar 구현',
-        'post page 구현',
-        'detail page 구현',
-        'post card 구현',
-      ],
-      repository: [
-        { where: '깃헙', url: 'https://github.com/codestates/My-subway' },
-        { where: '기획서', url: 'https://github.com/codestates/My-subway/wiki' },
-      ],
-    },
-  ];
-  const ref = useRef();
-  const [tz, setTz] = useState(0);
-  const [rotateY, setRotateY] = useState(0);
   const [idx, setIdx] = useState(0);
-  const [angle, setAngle] = useState(0);
-
-  useEffect(() => {
-    if (ref) {
-      const value = Math.round(ref.current.offsetHeight / 2 / Math.tan(Math.PI / ProjectsData.length));
-      setTz(value);
-      setRotateY(360 / ProjectsData.length);
-    }
-  }, []);
-
-  useEffect(() => {
-    let angleValue = (idx / ProjectsData.length) * -360;
-    setAngle(angleValue);
-  }, [idx]);
 
   return (
-    <SkillPageS>
+    <ProjectPage>
       <ProjectsContainer>
         <div className="projects">MY PROJECTS</div>
-        <InnerContainer angle={angle} transZ={tz}>
-          {ProjectsData.map((el, key) => {
-            return (
-              <Card3D ref={ref} key={key} angle={`${rotateY}` * key} transZ={tz}>
-                {key}
-              </Card3D>
-            );
-          })}
-        </InnerContainer>
+        <Card3DComponent idx={idx} />
         <ArrowLeft
           onClick={() => {
             setIdx((prev) => prev - 1);
@@ -175,7 +81,7 @@ function Projects({}) {
           ㅇ
         </ArrowRight>
       </ProjectsContainer>
-    </SkillPageS>
+    </ProjectPage>
   );
 }
 
