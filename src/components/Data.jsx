d03b1ceec6680403a321dd8f1a1e254cae577ab3
byungmin12 <kbm940526@naver.com>
2021-12-07 21:21:25 +0900

    project data

---
+++ src/components/Data.jsx
@@ -0,0 +1,56 @@
+export const ProjectsData = [
+  {
+    url: '../../newMyweb.gif',
+    title: 'PORTFOLIO 2.0',
+    explain: 'Three.js를 활용하여 만들 포트폴리오 사이트',
+    stack: ['React', 'STYLED COMPONENTS', 'JS', 'three.js'],
+    contribution: ['웹 전체적인 디자인 구축', '각종 Animation 지정', '3d 모델링'],
+    repository: [
+      { where: '배포', url: 'byungmin.kim' },
+      { where: '깃헙', url: 'https://github.com/byungmin12/MyWeb' },
+    ],
+  },
+  {
+    url: '../../mywebsize.gif',
+    title: 'PORTFOLIO 1.0',
+    explain: 'React와 Animation Css를 활용하여 프레젠테이션 형식으로 나를 소개하는 웹 사이트',
+    stack: ['React', 'STYLED COMPONENTS', 'JS'],
+    contribution: ['웹 전체적인 디자인 구축', '각종 Animation 지정'],
+    repository: [
+      { where: '배포', url: 'byungmin.kim' },
+      { where: '깃헙', url: 'https://github.com/byungmin12/MyWeb' },
+    ],
+  },
+  {
+    url: '../../isthisright.gif',
+    title: '이거맞아?',
+    explain: '운동을 좋아하는 사람들을 위한 운동 피드백 커뮤니티 ',
+    stack: ['React', 'STYLED COMPONENTS', 'JS'],
+    contribution: [
+      'Landing page 구현',
+      'Nav bar 구현',
+      'Question page 구현',
+      'Login page 구현',
+      'Sign up page 구현',
+      'Mypage 구현',
+      'footer 구현',
+    ],
+    repository: [
+      { where: '배포', url: 'isthisright.kr' },
+      { where: '깃헙', url: 'https://github.com/codestates/is-this-right' },
+      { where: '기획서', url: 'https://codestates.notion.site/1-S-M-S-31dbe334fe7544b79569c717c8c6bb16' },
+    ],
+  },
+  {
+    url: '../../mysubway.gif',
+    title: 'MySubway',
+    explain:
+      '나만의 서브웨이 메뉴를 만들어 저장하고 사람들과 공유합니다. 메뉴 추천을 통해 서브웨이 메뉴 고민을 줄여주는 웹페이지',
+    stack: ['React', 'STYLED COMPONENTS', 'JS'],
+    contribution: ['웹 전체적인 디자인 구축', 'Side bar 구현', 'post page 구현', 'detail page 구현', 'post card 구현'],
+    repository: [
+      { where: '깃헙', url: 'https://github.com/codestates/My-subway' },
+      { where: '기획서', url: 'https://github.com/codestates/My-subway/wiki' },
+    ],
+  },
+];
